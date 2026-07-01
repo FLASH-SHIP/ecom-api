e9a5708df006049f9af98d9672daa2e19bed574e
huynd <huy.ngo@flashtech.asia>
2026-07-01 09:42:35 +0700

    Feature customer: Update profile

---
+++ packages/ui/components/icons.tsx
@@ -0,0 +1,4 @@
+export { AppleIcon } from "./icon-component/AppleIcon";
+export { GoogleIcon } from "./icon-component/GoogleIcon";
+export { TelegramIcon } from "./icon-component/TelegramIcon";
+export { WhatsappIcon } from "./icon-component/WhatsappIcon";
