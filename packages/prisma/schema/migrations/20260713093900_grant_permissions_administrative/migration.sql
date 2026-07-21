84b61149d98e7257d673a31420397029f2d74ab4
huynd <huy.ngo@flashtech.asia>
2026-07-21 11:50:06 +0700

    Feature customer: Implementation division for single order

--- packages/prisma/schema/migrations/20260713093900_grant_permissions_administrative/migration.sql
+++ packages/prisma/schema/migrations/20260713093900_grant_permissions_administrative/migration.sql
@@ -1,11 +1,11 @@
 -- Grant permissions on administrative division tables to app user
-GRANT ALL PRIVILEGES ON TABLE provinces TO cms;
-GRANT ALL PRIVILEGES ON TABLE wards TO cms;
-GRANT USAGE, SELECT ON SEQUENCE provinces_id_seq TO cms;
-GRANT USAGE, SELECT ON SEQUENCE wards_id_seq TO cms;
+GRANT ALL PRIVILEGES ON TABLE provinces TO ecom;
+GRANT ALL PRIVILEGES ON TABLE wards TO ecom;
+GRANT USAGE, SELECT ON SEQUENCE provinces_id_seq TO ecom;
+GRANT USAGE, SELECT ON SEQUENCE wards_id_seq TO ecom;
 
 -- Ensure future tables/sequences created by migrations are auto-granted
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-  GRANT ALL PRIVILEGES ON TABLES TO cms;
+  GRANT ALL PRIVILEGES ON TABLES TO ecom;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-  GRANT ALL PRIVILEGES ON SEQUENCES TO cms;
+  GRANT ALL PRIVILEGES ON SEQUENCES TO ecom;
